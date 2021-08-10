683e1c6815626f64d99fc834c4274614908aa2a9
jacquescode <jojacquesqc@gmail.com>
2021-08-10 00:42:04 -0400

    update

--- app.js
+++ app.js
@@ -1,11 +1,83 @@
 // ****** SELECT ITEMS **********
 
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 // edit option
-
+let editElement;
+let editFlag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
+
+//submit form
+form.addEventListener("submit", addItem);
+// clear list
+clearBtn.addEventListener("click, clearItems");
+// display items onload
+window.addEventListener("DOMContentLoaded", setupItems);
 
 // ****** FUNCTIONS **********
 
+//add item
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createElement("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class"title">${value}</p>
+    <div class="btn-container">
+      <!-- edit btn -->
+      <button type="button" class="edit-btn">
+        <i class=fas fa-edit"></i>
+      </button>
+      <!-- delete btn -->
+      <button>type="button"  class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>  
+    `;
+    // add event listeners to both buttons;
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editItem);
+
+    // append child
+    list.appendChild(element);
+    //display alert
+    displayAlert("item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // set local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
+  } else if (value !== "" && editFlag) {
+    editElement.innerHTML = value;
+    displayAlert("value changed", "success");
+
+    //edit local storage
+    editLocalStorage(editID, value);
+    setBackToDefault();
+  } else {
+    displayAlert("please enter value", "danger");
+  }
+}
+// display displayAlert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
